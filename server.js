19fd97497aad8a24a4338c95557c4dae5a36ce1b
Cody Brown <cody@brightinfinite.com>
2022-04-08 02:08:46 -0500

    setup read route, tested get

--- server.js
+++ server.js
@@ -15,7 +15,22 @@ app.listen(port, () => {
 
 // CREATE
 app.post('/users', (req, res) => {
-	// User.create()
+	User.create(
+		{
+			name: req.body.newData.name,
+			email: req.body.newData.email,
+			password: req.body.newData.password,
+		},
+		(err, data) => {
+			if (err) {
+				res.json({ success: false, message: err })
+			} else if (!data) {
+				res.json({ success: false, message: 'Not Found' })
+			} else {
+				res.json({ success: true, data: data })
+			}
+		}
+	)
 })
 
 // Route chaining is used since all crud routes use same endpoint /users/:id
@@ -23,7 +38,24 @@ app
 	.route('/users/:id')
 	// READ
 	.get((req, res) => {
-		// User.findById()
+		User.findById(req.params.id, (err, data) => {
+			if (err) {
+				res.json({
+					success: false,
+					message: err,
+				})
+			} else if (!data) {
+				res.json({
+					success: false,
+					message: 'Not Found',
+				})
+			} else {
+				res.json({
+					success: true,
+					data: data,
+				})
+			}
+		})
 	})
 	// UPDATE
 	.put((req, res) => {
